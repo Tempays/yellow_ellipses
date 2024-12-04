91ae57bc08a1815f8303494bee26fc45d600432b
Tempays <nazarenkotimofey2008@gmail.com>
2024-12-04 14:22:11 +0500

    Итоговая программа

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+import io
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt6 import uic
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QWidget, QApplication
+from random import randint
 
 
-# Press the green button in the gutter to run the script.
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.setFixedSize(615, 500)
+        self.button.clicked.connect(self.paint)
+        self.paint_ = False
+
+    def paintEvent(self, event):
+        if self.paint_:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_ellipse(qp)
+            qp.end()
+        self.paint_ = False
+
+    def paint(self):
+        self.paint_ = True
+        self.update()
+
+    def draw_ellipse(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        number = randint(1, 7)
+        for _ in range(number):
+            radius = randint(20, 500)
+            qp.drawEllipse(randint(-50, 450), randint(-50, 450), radius, radius)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    wnd = Widget()
+    wnd.show()
+    sys.exit(app.exec())
